5278169528477965e010e077f8876b384395ccd7
robert1564 <robertmaximus383@gmail.com>
2022-05-15 23:28:39 +0300

    travis update

--- src/util/index.js
+++ src/util/index.js
@@ -4,7 +4,7 @@ import jwtDecode from "jwt-decode"
 const isDevelopment = window.location.hostname.includes("localhost");
 
 const getServer = () =>{
-    return isDevelopment ? "http://localhost:5000" : "";
+    return isDevelopment ? "http://localhost:5000" : "https://named-haven-350008.ey.r.appspot.com/";
 }
 
 const decodeUser = () => {
