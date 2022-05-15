8e1230a3139302ecb54e9263991f086c1ea40890
robert1564 <robertmaximus383@gmail.com>
2022-05-15 23:47:25 +0300

    travis update bug fix2

--- src/components/general/Product.js
+++ src/components/general/Product.js
@@ -1,41 +1,38 @@
-import React from 'react'
-import { Card, Button } from 'antd';
+import React from "react";
+import { Card, Button } from "antd";
 import propTypes from "prop-types";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const { Meta } = Card;
 
-const Product = ({ product, description, buttonName, link }) => {
-    return (
-        <div style={{padding: "10px"}}>
-            <Link to={link}>
-                <Card
-                    hoverable
-                    style={{ width: 300 }}
-                    cover={
-                        <img
-                            alt="example"
-                            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                        />
-                    }
-                >
-                    <Meta
-                        title={product.name}
-                        description={description}
-                    />
-                    <Button type="primary">{buttonName}</Button>
-                </Card>
-            </Link>
-
-        </div>
-    )
-}
+const Product = ({ product, description, link }) => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <Link to={link}>
+        <Card
+          hoverable
+          style={{ width: 300 }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+        >
+          <Meta title={product.name} description={description} />
+          <Link className="btn btn-primary" to={uploadImages}>
+            Add Images
+          </Link>
+        </Card>
+      </Link>
+    </div>
+  );
+};
 
 Product.propTypes = {
-    product: propTypes.object.isRequired,
-    description: propTypes.func.isRequired,
-    buttonName: propTypes.string,
-}
+  product: propTypes.object.isRequired,
+  description: propTypes.func.isRequired,
+  buttonName: propTypes.string,
+};
 
 export default Product;
-
